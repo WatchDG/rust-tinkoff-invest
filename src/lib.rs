7948e539250504c7ff833b9aaa7f74e7be12d7fe
WatchDG <baranov@w3m.co>
2021-02-15 14:56:10 +0300

    add method get_market_instrument_by_ticker

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate bytes;
 extern crate hyper;
 extern crate hyper_tls;
@@ -11,76 +13,30 @@ use hyper::{
     Body, Method, Request, StatusCode, Uri,
 };
 use hyper_tls::HttpsConnector;
-use serde::Deserialize;
+
+mod types;
+
+use types::{TinkoffInstrument, TinkoffInstruments, TinkoffResponseData};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-const BASE_URL: &str = "https://api-invest.tinkoff.ru/openapi";
-
-#[derive(Deserialize, Debug)]
-pub enum Currency {
-    RUB,
-    USD,
-    EUR,
-    GBP,
-    HKD,
-    CHF,
-    JPY,
-    CNY,
-    TRY,
-}
-
-#[derive(Deserialize, Debug)]
-pub enum InstrumentType {
-    Stock,
-    Currency,
-    Bond,
-    Etf,
-}
-
-pub struct TinkoffInvest {
-    client: Client<HttpsConnector<HttpConnector>>,
-    base_url: &'static str,
-    token: &'static str,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct TinkoffInstrument {
-    pub figi: String,
-    pub ticker: String,
-    pub name: String,
-    pub isin: Option<String>,
-    #[serde(rename(serialize = "minPriceIncrement", deserialize = "minPriceIncrement"))]
-    pub min_price_increment: Option<f64>,
-    pub lot: u64,
-    pub currency: Option<Currency>,
-    #[serde(rename(serialize = "type", deserialize = "type"))]
-    pub type_: InstrumentType,
-}
-
-#[derive(Deserialize, Debug)]
-struct TinkoffInstruments {
-    instruments: Vec<TinkoffInstrument>,
-}
-
-#[derive(Deserialize, Debug)]
-struct TinkoffResponseData<P> {
-    #[serde(rename(serialize = "trackingId", deserialize = "trackingId"))]
-    tracking_id: String,
-    payload: P,
-    status: String,
-    error: Option<String>,
+lazy_static! {
+    static ref BASE_URI: &'static str = "https://api-invest.tinkoff.ru/openapi";
+    static ref GET_STOCKS_URI: Uri = (BASE_URI.to_owned() + "/market/stocks").parse::<Uri>().unwrap();
+    static ref GET_BONDS_URI: Uri = (BASE_URI.to_owned() + "/market/bonds").parse::<Uri>().unwrap();
+    static ref GET_ETFS_URI: Uri = (BASE_URI.to_owned() + "/market/etfs").parse::<Uri>().unwrap();
+    static ref GET_CURRENCIES_URI: Uri = (BASE_URI.to_owned() + "/market/currencies").parse::<Uri>().unwrap();
 }
 
 async fn request_get(
     client: &Client<HttpsConnector<HttpConnector>>,
     uri: &Uri,
-    token: &str,
+    auth: &str,
 ) -> Result<BytesMut> {
     let request = Request::builder()
         .method(Method::GET)
         .uri(uri)
-        .header("Authorization", "Bearer ".to_owned() + token)
+        .header("Authorization", auth)
         .body(Body::empty())?;
     let mut response = client.request(request).await?;
     let status = response.status();
@@ -95,20 +51,24 @@ async fn request_get(
     Ok(body)
 }
 
+pub struct TinkoffInvest {
+    client: Client<HttpsConnector<HttpConnector>>,
+    auth: String,
+}
+
 impl TinkoffInvest {
     pub fn new(token: &'static str) -> TinkoffInvest {
         let https = HttpsConnector::new();
         let client = Client::builder().build(https);
+        let auth = "Bearer ".to_owned() + token;
         TinkoffInvest {
             client,
-            base_url: BASE_URL,
-            token,
+            auth,
         }
     }
 
     pub async fn get_stocks(&self) -> Result<Vec<TinkoffInstrument>> {
-        let uri = (self.base_url.to_owned() + "/market/stocks").parse::<Uri>()?;
-        let response_data = request_get(&self.client, &uri, self.token).await?;
+        let response_data = request_get(&self.client, &GET_STOCKS_URI, self.auth.as_str()).await?;
         let data = serde_json::from_slice::<TinkoffResponseData<TinkoffInstruments>>(
             response_data.as_ref(),
         )?;
@@ -116,8 +76,7 @@ impl TinkoffInvest {
     }
 
     pub async fn get_bonds(&self) -> Result<Vec<TinkoffInstrument>> {
-        let uri = (self.base_url.to_owned() + "/market/bonds").parse::<Uri>()?;
-        let response_data = request_get(&self.client, &uri, self.token).await?;
+        let response_data = request_get(&self.client, &GET_BONDS_URI, self.auth.as_str()).await?;
         let data = serde_json::from_slice::<TinkoffResponseData<TinkoffInstruments>>(
             response_data.as_ref(),
         )?;
@@ -125,8 +84,7 @@ impl TinkoffInvest {
     }
 
     pub async fn get_etfs(&self) -> Result<Vec<TinkoffInstrument>> {
-        let uri = (self.base_url.to_owned() + "/market/etfs").parse::<Uri>()?;
-        let response_data = request_get(&self.client, &uri, self.token).await?;
+        let response_data = request_get(&self.client, &GET_ETFS_URI, self.auth.as_str()).await?;
         let data = serde_json::from_slice::<TinkoffResponseData<TinkoffInstruments>>(
             response_data.as_ref(),
         )?;
@@ -134,8 +92,16 @@ impl TinkoffInvest {
     }
 
     pub async fn get_currencies(&self) -> Result<Vec<TinkoffInstrument>> {
-        let uri = (self.base_url.to_owned() + "/market/currencies").parse::<Uri>()?;
-        let response_data = request_get(&self.client, &uri, self.token).await?;
+        let response_data = request_get(&self.client, &GET_CURRENCIES_URI, self.auth.as_str()).await?;
+        let data = serde_json::from_slice::<TinkoffResponseData<TinkoffInstruments>>(
+            response_data.as_ref(),
+        )?;
+        Ok(data.payload.instruments)
+    }
+
+    pub async fn get_market_instrument_by_ticker(&self, ticker: &str) -> Result<Vec<TinkoffInstrument>> {
+        let uri = (BASE_URI.to_owned() + "/market/search/by-ticker?ticker=" + ticker).parse::<Uri>()?;
+        let response_data = request_get(&self.client, &uri, self.auth.as_str()).await?;
         let data = serde_json::from_slice::<TinkoffResponseData<TinkoffInstruments>>(
             response_data.as_ref(),
         )?;
---
+++ src/types/mod.rs
@@ -0,0 +1,50 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub enum Currency {
+    RUB,
+    USD,
+    EUR,
+    GBP,
+    HKD,
+    CHF,
+    JPY,
+    CNY,
+    TRY,
+}
+
+#[derive(Deserialize, Debug)]
+pub enum InstrumentType {
+    Stock,
+    Currency,
+    Bond,
+    Etf,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TinkoffInstrument {
+    pub figi: String,
+    pub ticker: String,
+    pub name: String,
+    pub isin: Option<String>,
+    #[serde(rename(serialize = "minPriceIncrement", deserialize = "minPriceIncrement"))]
+    pub min_price_increment: Option<f64>,
+    pub lot: u64,
+    pub currency: Option<Currency>,
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub type_: InstrumentType,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TinkoffInstruments {
+    pub instruments: Vec<TinkoffInstrument>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TinkoffResponseData<P> {
+    #[serde(rename(serialize = "trackingId", deserialize = "trackingId"))]
+    pub tracking_id: String,
+    pub payload: P,
+    pub status: String,
+    pub error: Option<String>,
+}
