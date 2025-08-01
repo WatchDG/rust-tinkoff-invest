617a4fa60fb090952b5cc6264aaab758f10b49a6
WatchDG <baranov@w3m.co>
2025-08-01 22:36:34 +0300

    doc: figi and isin

--- src/types/figi.rs
+++ src/types/figi.rs
@@ -1,9 +1,36 @@
 use crate::traits;
 
+/// FIGI (Financial Instrument Global Identifier) - глобальный идентификатор финансового инструмента.
+///
+/// FIGI - это стандарт идентификации финансовых инструментов, разработанный Bloomberg.
+/// Каждый FIGI представляет собой уникальную строку, которая идентифицирует конкретный
+/// финансовый инструмент (акции, облигации, фонды и т.д.) на глобальном уровне.
+///
+/// # Примеры
+///
+/// ```rust
+/// use tinkoff_invest::types::Figi;
+///
+/// let figi = Figi::new("BBG000B9XRY4".to_string());
+/// let figi_from_str: Figi = "BBG000B9XRY4".into();
+/// ```
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Figi(String);
 
 impl Figi {
+    /// Создает новый экземпляр FIGI из строки.
+    ///
+    /// # Аргументы
+    ///
+    /// * `figi` - строка, содержащая FIGI идентификатор
+    ///
+    /// # Примеры
+    ///
+    /// ```rust
+    /// use tinkoff_invest::types::Figi;
+    ///
+    /// let figi = Figi::new("BBG000B9XRY4".to_string());
+    /// ```
     #[inline]
     pub fn new(figi: String) -> Self {
         Self(figi)
