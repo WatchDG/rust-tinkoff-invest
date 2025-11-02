469d21bbb41284a526b127d3b7e3a4c7e8fea832
WatchDG <baranov@w3m.co>
2025-11-02 18:01:10 +0300

    refactor: change token handling to use MetadataValue for authorization

--- src/interceptor.rs
+++ src/interceptor.rs
@@ -1,25 +1,28 @@
-use tonic::{Request, Status, service::Interceptor};
+use tonic::{
+    Request, Status,
+    metadata::{Ascii, MetadataValue},
+    service::Interceptor,
+};
 use uuid::Uuid;
 
 #[derive(Debug, Clone)]
 pub struct TinkoffInvestInterceptor {
-    token: String,
+    authorization: MetadataValue<Ascii>,
 }
 
 impl TinkoffInvestInterceptor {
     #[inline]
     pub fn new(token: String) -> Self {
-        Self { token }
+        let authorization = format!("bearer {}", token).parse().unwrap();
+        Self { authorization }
     }
 }
 
 impl Interceptor for TinkoffInvestInterceptor {
     fn call(&mut self, request: Request<()>) -> Result<Request<()>, Status> {
         let mut req = request;
-        req.metadata_mut().append(
-            "authorization",
-            format!("bearer {}", self.token).parse().unwrap(),
-        );
+        req.metadata_mut()
+            .append("authorization", self.authorization.clone());
         req.metadata_mut()
             .append("x-tracking-id", Uuid::new_v4().to_string().parse().unwrap());
         Ok(req)
