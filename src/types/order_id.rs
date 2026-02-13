8f43ecaffd951c7b1d47b6e338f044929b3900ab
WatchDG <baranov@w3m.co>
2026-02-13 13:53:36 +0300

    refactor: add lull feature and implement ReadOrderIdInnerRef for OrderId to enhance functionality

--- src/types/order_id.rs
+++ src/types/order_id.rs
@@ -12,3 +12,13 @@ impl From<String> for OrderId {
         Self(value)
     }
 }
+
+#[cfg(feature = "lull")]
+pub use lull_spec;
+
+#[cfg(feature = "lull")]
+impl lull_spec::ReadOrderIdInnerRef<String> for OrderId {
+    fn read_order_id_inner_ref(&self) -> &String {
+        &self.0
+    }
+}
