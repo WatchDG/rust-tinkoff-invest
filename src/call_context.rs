41168b156537b3b9dc500797c8273501c5f66e30
WatchDG <baranov@w3m.co>
2026-02-14 19:18:27 +0300

    refactor: enhance TCallContext by adding with_account and with_order methods for improved context creation

--- src/call_context.rs
+++ src/call_context.rs
@@ -51,6 +51,15 @@ impl TAccountContext {
         self.request_id = request_id;
         self
     }
+
+    /// Создаёт [`TOrderContext`] с заданным order_id
+    pub fn with_order(self, order_id: impl traits::ToOrderId) -> TOrderContext {
+        TOrderContext {
+            request_id: self.request_id,
+            account_id: self.account_id,
+            order_id: order_id.to_order_id(),
+        }
+    }
 }
 
 impl traits::RequestId for TAccountContext {
@@ -109,25 +118,22 @@ impl traits::ToOrderIdRef for TOrderContext {
     }
 }
 
-/// Контекст вызова, содержащий информацию для идентификации запроса (универсальный, все поля опциональны).
+/// Контекст вызова с опциональными полями (только для запросов, требующих request_id).
+///
+/// Для запросов с account_id используйте [`TAccountContext`].
+/// Для запросов с account_id и order_id используйте [`TOrderContext`].
+///
+/// Компилятор гарантирует наличие обязательных полей через типы контекста.
 #[derive(Debug, Clone)]
 pub struct TCallContext {
     /// Идентификатор запроса (x-tracking-id) (опционально)
     pub request_id: Option<String>,
-    /// Идентификатор аккаунта (опционально)
-    pub account_id: Option<types::AccountId>,
-    /// Идентификатор ордера (опционально)
-    pub order_id: Option<types::OrderId>,
 }
 
 impl TCallContext {
     /// Создает новый контекст
     pub fn new() -> Self {
-        Self {
-            request_id: None,
-            account_id: None,
-            order_id: None,
-        }
+        Self { request_id: None }
     }
 
     /// Устанавливает request_id
@@ -136,54 +142,12 @@ impl TCallContext {
         self
     }
 
-    /// Устанавливает account_id из типа, реализующего ToAccountId
-    pub fn set_account_id<T>(&mut self, account: Option<T>) -> &mut Self
-    where
-        T: traits::ToAccountId,
-    {
-        self.account_id = account.map(|a| a.to_account_id());
-        self
-    }
-
-    /// Устанавливает order_id из типа, реализующего ToOrderId
-    pub fn set_order_id<T>(&mut self, order: Option<T>) -> &mut Self
-    where
-        T: traits::ToOrderId,
-    {
-        self.order_id = order.map(|o| o.to_order_id());
-        self
-    }
-}
-
-impl traits::ToAccountId for TCallContext {
-    fn to_account_id(&self) -> types::AccountId {
-        self.account_id
-            .clone()
-            .expect("account_id must be set in TCallContext to use ToAccountId")
-    }
-}
-
-impl traits::ToAccountIdRef for TCallContext {
-    fn to_account_id_ref(&self) -> &types::AccountId {
-        self.account_id
-            .as_ref()
-            .expect("account_id must be set in TCallContext to use ToAccountIdRef")
-    }
-}
-
-impl traits::ToOrderId for TCallContext {
-    fn to_order_id(&self) -> types::OrderId {
-        self.order_id
-            .clone()
-            .expect("order_id must be set in TCallContext to use ToOrderId")
-    }
-}
-
-impl traits::ToOrderIdRef for TCallContext {
-    fn to_order_id_ref(&self) -> &types::OrderId {
-        self.order_id
-            .as_ref()
-            .expect("order_id must be set in TCallContext to use ToOrderIdRef")
+    /// Создаёт [`TAccountContext`] с заданным account_id
+    pub fn with_account(self, account_id: impl traits::ToAccountId) -> TAccountContext {
+        TAccountContext {
+            request_id: self.request_id,
+            account_id: account_id.to_account_id(),
+        }
     }
 }
 
